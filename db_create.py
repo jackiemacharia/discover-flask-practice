bdef26917c4a7fa5a5a8fd6b50b242e311ae2284
Jackie Macharia <jaxmacharia@gmail.com>
2016-10-29 15:47:58 +0300

    Add comments on how to integrate sqlite to heroku postgresql

--- db_create.py
+++ db_create.py
@@ -10,3 +10,5 @@ db.session.add(BlogPost("Well", "I\'m well."))
 
 # commit the changes
 db.session.commit()
+
+# pip install psycopg2 to add sqlite data to heroku postgersql then heroku run python db_create.py
