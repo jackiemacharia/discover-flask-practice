4ed863c8dfbf0e18e0182173b7b90bd0e040cbe6
Jackie Macharia <jaxmacharia@gmail.com>
2016-11-06 23:51:38 +0300

    Add migrations

---
+++ manage.py
@@ -0,0 +1,21 @@
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+import os
+
+from app import app, db
+
+app.config.from_object(os.environ['APP_SETTINGS'])
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
+
+
+# python manage.py db init - creates migrations folder
+
+# python manage.py db migrate - creates a migrations scripts
+
+# python manage.py db upgrade
