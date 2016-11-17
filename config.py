a98a133e24c9ee3915813a05f223e44e2bd8092f
Jackie Macharia <jaxmacharia@gmail.com>
2016-11-17 18:32:51 +0300

    Add flask testing

--- config.py
+++ config.py
@@ -8,7 +8,15 @@ class BaseConfig(object):
     SECRET_KEY = 'Hb\x8b\xbbn\xf2\x9d\xe1Q\xc2\xe1,h;\x8c\xbb\xdb\xed\xe6\xc4\xd1W \x86'  # use a random key generator command: import os. os.urandom(n)  # not advisable on a public repo
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']  # sets up a local environment variable for db command: export DATABASE_URL="sqlite:///posts.db" with postgres: export DATABASE_URL="postgresql:///discover_flask_final" where discover_flask_final is the db name
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    print SQLALCHEMY_DATABASE_URI
+    # print SQLALCHEMY_DATABASE_URI
+
+
+class TestConfig(BaseConfig):
+    DEBUG = True
+    TESTING = True
+    WTF_CSRF_ENABLED = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+
 
 # for major projects, do not add your config file to a public repo  # Also if you push a config file with different settings for each environment without using the class structure,it overides previous settings each time establishing different environment settings
 # access db: sudo -u postgres psql postgres
